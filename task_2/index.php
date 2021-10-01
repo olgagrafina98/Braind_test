78e71834dd8ae51a4486e19865a7c88d88290dfe
olgagrafina98 <53266380+olgagrafina98@users.noreply.github.com>
2021-10-01 16:33:49 +0300

    Add files via upload

---
+++ task_2/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Задача 2</title>
+    </head>
+    <body>
+        <form action="task_2.php" method="post">
+        <input type="number" name="fatal_errors" placeholder="Количество фатальных ошибок">
+        <input type="number" name="warnings" placeholder="Количество ворнингов">
+        <button type="submit">Отправить</button>
+        </form>
+    </body>
+    </html>
